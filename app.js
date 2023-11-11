3c345650e15dd78b62372ab5efda2449980beb95
Adrian Lee Jackson <adrian.jackson@mymedsandme.com>
2023-11-11 21:19:24 +0000

    Use midi

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let score = 0;
-let inputMode = 'keyboard'; // 'keyboard' or 'midi'
+let inputMode = 'midi'; // 'keyboard' or 'midi'
 const trebleNotes = ['C/4', 'D/4', 'E/4', 'F/4', 'G/4', 'A/4', 'B/4', 'C/5', 'D/5', 'E/5', 'F/5', 'G/5', 'A/5', 'B/5', 'C/6'];
 const bassNotes = ['C/2', 'D/2', 'E/2', 'F/2', 'G/2', 'A/2', 'B/2', 'C/3', 'D/3', 'E/3', 'F/3', 'G/3', 'A/3', 'B/3', 'C/4'];
 let currentNote, currentClef;
